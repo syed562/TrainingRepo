0bf8572827a0303ca8a8af7d5dadb8ee19602892
Dev2 <Dev2@gmail.com>
2025-11-27 17:22:18 +0530

    new proj for microservices_quizz app

---
+++ Boot5_Microservices_01QuizApp/src/main/java/com/app/Boot5Microservices01QuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot5Microservices01QuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot5Microservices01QuizAppApplication.class, args);
+	}
+
+}
