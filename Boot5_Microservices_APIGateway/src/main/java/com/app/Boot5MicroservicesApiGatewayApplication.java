1091e52526648b263a096ef3617603e9f935bd31
Dev2 <Dev2@gmail.com>
2025-11-28 20:41:01 +0530

    APIGATEWAY developed

---
+++ Boot5_Microservices_APIGateway/src/main/java/com/app/Boot5MicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot5MicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot5MicroservicesApiGatewayApplication.class, args);
+	}
+
+}
